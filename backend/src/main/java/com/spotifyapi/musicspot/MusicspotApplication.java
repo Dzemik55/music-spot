3d510ce1fe6768c51c1287dbbcecc8feeabbf209
tomasz.rycko <tomasz.rycko@gmail.com>
2025-01-11 14:51:47 +0100

    initial commit

---
+++ backend/src/main/java/com/spotifyapi/musicspot/MusicspotApplication.java
@@ -0,0 +1,13 @@
+package com.spotifyapi.musicspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicspotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicspotApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/spotifyapi/musicspot/MusicspotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spotifyapi.musicspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicspotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
